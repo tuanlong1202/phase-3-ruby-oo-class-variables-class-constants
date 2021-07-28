c9bd099f0be22ceed2817c5de551b9122bea3b00
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-27 22:22:34 -0400

    Phase 3 ruby oo class variables class constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
-    @brand = brand
+    self.brand = brand
   end
 
   def cobble
@@ -11,4 +13,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand_name)
+    @brand = brand_name
+    if !BRANDS.include?(brand_name)
+      BRANDS << brand_name
+    end
+  end
 end
